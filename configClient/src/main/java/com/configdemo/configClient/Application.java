4971e8adcaef48b26959e5915234b43947e332dd
ScyBernate <244395026@qq.com>
2016-12-21 17:52:13 +0800

    配置中心客户端

---
+++ configClient/src/main/java/com/configdemo/configClient/Application.java
@@ -0,0 +1,31 @@
+package com.configdemo.configClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@RestController
+public class Application {
+    @Value("${username}")
+    String username;
+    
+    @Value("${password}")
+    String password;
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello " + username + "!" + "password:"+password;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
