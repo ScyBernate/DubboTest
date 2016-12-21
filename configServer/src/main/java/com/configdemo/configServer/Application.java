0cd27b0a994ea2cd748e04a169edeb4e50c52aa5
ScyBernate <244395026@qq.com>
2016-12-21 17:53:15 +0800

    配置中心服务器端

---
+++ configServer/src/main/java/com/configdemo/configServer/Application.java
@@ -0,0 +1,19 @@
+package com.configdemo.configServer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+}
