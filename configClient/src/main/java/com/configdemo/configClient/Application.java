635996260df78e53cdaa9fe9079097b817f7afb6
ScyBernate <244395026@qq.com>
2016-12-21 18:18:56 +0800

    获取git配置中心仓库地址修改

--- configClient/src/main/java/com/configdemo/configClient/Application.java
+++ configClient/src/main/java/com/configdemo/configClient/Application.java
@@ -3,14 +3,16 @@ package com.configdemo.configClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 /**
  * Hello world!
  *
  */
-
 @SpringBootApplication
 @RestController
 public class Application {
@@ -26,6 +28,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	SpringApplication.run(Application.class, args);
     }
 }
