f586e6b0506e047d10733982375fabe438f938a1
ScyBernate <244395026@qq.com>
2016-12-22 10:00:40 +0800

    增加注释

--- configClient/src/main/java/com/configdemo/configClient/controller/TestController.java
+++ configClient/src/main/java/com/configdemo/configClient/controller/TestController.java
@@ -4,6 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * <p>类名：TestController.java</p> 
+ * <p>描述：获取配置文件属性</p>
+ * <p>作者： Administrator</p>
+ * <p>日期：2016年12月22日</p>
+ *
+ */
 @RestController
 public class TestController {
 
